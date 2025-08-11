58dd338b050e7dfe7ecd89ef68e728a71cb6376b
ffgan <sudoemt@gmail.com>
2025-08-11 22:00:53 +0800

    init repo
    Now Chacra can basically run in a container.
    
    other info
    Co-authored by: nijincheng@iscas.ac.cn;

---
+++ chacra_image/main.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# env is pass by dockerfile
+cd ${APP_HOME}/src/${APP_NAME}/${APP_NAME}
+
+sleep 10
+${APP_HOME}/bin/celery multi start 5 -Q:1,2 poll_repos,celery -Q:3-5 build_repos -A asynch --logfile=/var/log/celery/%n%I.log
+
+${APP_HOME}/bin/celery -A asynch beat --loglevel=info &
+
+/usr/sbin/nginx -g 'daemon off;' &
+
+
+sleep 10
+sudo bash ${APP_HOME}/init-chacra-app.sh
+
+${APP_HOME}/bin/gunicorn_pecan -w 10 -t 1200 ${APP_HOME}/src/${APP_NAME}/prod.py
---
+++ chacra_image/prod_api_creds.py
@@ -0,0 +1,3 @@
+# Basic HTTP Auth credentials
+api_user = "jack"
+api_key = "xxxxxx666666"
